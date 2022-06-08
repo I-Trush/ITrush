be5d112d6872da3ff6f69a7e156a0093bd39c80a
I-Trush <gari-man@yandex.ru>
2022-06-08 13:41:20 +0300

    11

---
+++ my1.py
@@ -0,0 +1,12 @@
+def func1(a):
+    for i in range(1, 7):
+        print(i)
+        a = a * i
+        print(a)
+
+
+def funcx():
+    func1()
+
+
+func1(8)
---
+++ my2.py
@@ -0,0 +1,8 @@
+
+s = 'hello'
+
+import my1
+
+print(s)
+my1.func1()
+gfhdgdgf
